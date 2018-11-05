018a941ed6a169ebbfb3e3cb3f66701200f5976c
plmntnsv <plamen@Plamens-Mac-mini.local>
2018-11-05 17:25:11 +0200

    Stanford Lesson 1 finished

---
+++ Concetration/Concetration/Concentration.swift
@@ -0,0 +1,17 @@
+//
+//  Concentration.swift
+//  Concetration
+//
+//  Created by Plamen on 5.11.18.
+//  Copyright © 2018 Plamen. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    var cards: [Card]
+    
+    func chooseCard(at index:Int){
+        
+    }
+}
