3078db77678d0a276d6718c24f96b26b30187e4a
plmntnsv <plamen@Plamens-Mac-mini.local>
2018-11-08 19:15:39 +0200

    Some game logic implemented for  replacing active matched sets with cards from the all unused decks

--- Concetration/Concetration/ViewController.swift
+++ Concetration/Concetration/ViewController.swift
@@ -47,7 +47,7 @@ class ViewController: UIViewController {
             if card.isFaceUp{
                 button.setTitle(emoji(for: card), for: UIControl.State.normal)
                 button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            } else{
+            } else {
                 button.setTitle("", for: UIControl.State.normal)
                 button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
             }
--- Set/Set/ViewController.swift
+++ Set/Set/ViewController.swift
@@ -10,24 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
     private(set) var game = SetGame()
-    @IBOutlet var cardButtons: [UIButton]!
+    
+    private var currentlySelectedCardButtons = [UIButton]()
+    
+    @IBOutlet var activeCardButtons: [UIButton]!
+    @IBOutlet var inactiveCardButtons: [UIButton]!
+    
     @IBOutlet weak var dealCardsBtn: UIButton!
     @IBOutlet weak var pointsLabel: UILabel!
     
     @IBAction func selectCard(_ sender: UIButton) {
-        if sender.layer.borderWidth == 0 {
-            sender.layer.borderWidth = 3.0
-            sender.layer.borderColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        
+        if currentlySelectedCardButtons.count < 3 {
+            if sender.layer.borderWidth == 0 {
+                select(sender)
+                currentlySelectedCardButtons.append(sender)
+            } else {
+                deselect(sender)
+                currentlySelectedCardButtons.remove(at: currentlySelectedCardButtons.firstIndex(of: sender)!)
+            }
         } else {
-            sender.layer.borderWidth = 0
-            sender.layer.borderColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 0)
+            currentlySelectedCardButtons.forEach() { $0.layer.borderWidth = 0; $0.layer.borderColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 0) }
+            currentlySelectedCardButtons.removeAll()
+            select(sender)
+            currentlySelectedCardButtons.append(sender)
+        }
+    }
+    
+    @IBAction func dealThreeMoreCardsClicked(_ sender: UIButton) {
+        // TODO: impl deal tree more cards func
+        if currentlySelectedCardButtons.count == 3 {
+            // TODO: check if they are a match
+            currentlySelectedCardButtons.forEach {
+                let card = inactiveCardButtons.removeFirst()
+                activeCardButtons.replace(element: card, at: activeCardButtons.firstIndex(of: $0)!)
+            }
         }
         
     }
     
-    @IBAction func dealThreeMoreCardsClicked(_ sender: UIButton) {
+    @IBAction func newGameClicked(_ sender: UIButton) {
+        // TODO: impl new game func
     }
     
-    @IBAction func newGameClicked(_ sender: UIButton) {
+    private func select(_ btn: UIButton){
+        btn.layer.borderWidth = 3.0
+        btn.layer.borderColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+    }
+    
+    private func deselect(_ btn: UIButton) {
+        btn.layer.borderWidth = 0
+        btn.layer.borderColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 0)
+    }
+}
+
+extension Array {
+    mutating func replace(element: Element, at index: Int) {
+        self.remove(at: index)
+        self.insert(element, at: index+1)
     }
 }
