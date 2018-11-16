60ac3188559b6680ca6aa2104f1d5abf40d762ce
plmntnsv <plamen.atanasov91@gmail.com>
2018-11-16 14:02:47 +0200

    just another easy task

--- telerik-judge-tasks/Telerik-Tasks/Telerik-Tasks/ViewController.swift
+++ telerik-judge-tasks/Telerik-Tasks/Telerik-Tasks/ViewController.swift
@@ -15,6 +15,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnClick(_ sender: UIButton) {
+        let n = 13
+        let input = "4114234412493"
+        var numbers = [Int:Int]()
+        
+        for i in input.indices {
+            let number = Int(String(input[i]))!
+            
+            if !numbers.keys.contains(number) {
+                numbers[number] = 1
+            } else {
+                numbers[number] = numbers[number]! + 1
+            }
+        }
+        let max = numbers.max { a, b in a.value < b.value }
+        print(max)
+    }
+    
 }
 
