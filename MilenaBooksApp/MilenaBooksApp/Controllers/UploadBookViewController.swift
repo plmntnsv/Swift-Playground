f6ff9b9af6d8b63a92ff348b697cf1bbcf0480fa
plmntnsv <plamen.atanasov91@gmail.com>
2018-11-28 13:16:36 +0200

    Uploading with user input data working

--- MilenaBooksApp/MilenaBooksApp/Controllers/UploadBookViewController.swift
+++ MilenaBooksApp/MilenaBooksApp/Controllers/UploadBookViewController.swift
@@ -13,22 +13,55 @@ import AlamofireObjectMapper
 class UploadBookViewController: UIViewController {
     @IBOutlet var uploadBookView: UploadBookView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // TODO: use SwiftValidator lib
+    private var validBook: Bool {
+        get {
+            if (uploadBookView.titleTextField.text?.isEmpty)! {
+                (uploadBookView.uploadButton as? ActivityButtonView)?.hideLoading()
+                return false
+            }
+            
+            if (uploadBookView.authorTextField.text?.isEmpty)! {
+                (uploadBookView.uploadButton as? ActivityButtonView)?.hideLoading()
+                return false
+            }
+            
+            if (uploadBookView.priceTextField.text?.isEmpty)! || Double((uploadBookView.priceTextField.text)!) == nil {
+                (uploadBookView.uploadButton as? ActivityButtonView)?.hideLoading()
+                return false
+            }
+            
+            if (uploadBookView.ratingTextField.text?.isEmpty)! || Int((uploadBookView.priceTextField.text)!) == nil {
+                (uploadBookView.uploadButton as? ActivityButtonView)?.hideLoading()
+                return false
+            }
+            
+            return true
+        }
     }
     
     @IBAction func uploadBookBtnClicked(_ sender: Any) {
         if let btn = sender as? ActivityButtonView {
             if !btn.isUploading {
                 btn.showLoading()
-                uploadBook()
+                if validBook {
+                    let title = uploadBookView.titleTextField.text!
+                    let author = uploadBookView.authorTextField.text!
+                    let price = Double(uploadBookView.priceTextField.text!)!
+                    let rating = Int(uploadBookView.ratingTextField.text!)!
+                    let url = uploadBookView.coverImageUrlTextField.text
+                    let desc = uploadBookView.descriptionTextView.text
+                    let book = UploadBook(title: title, price: price, author: author, rating: rating, coverImageUrl: url, description: desc)
+                    
+                    upload(bookToUpload: book)
+                }
             } else {
                 print("currently uploading")
             }
         }
     }
     
-    private func uploadBook() {
+    private func upload(bookToUpload book: UploadBook) {
         let urlString = "http://milenabooks.azurewebsites.net/api/books"
         //                let headers: HTTPHeaders = [
         //                    "Content-Type": "application/x-www-form-urlencoded",
@@ -46,15 +79,18 @@ class UploadBookViewController: UIViewController {
         
         Alamofire.request(urlString,
                           method: .post,
-                          parameters: BookMockData.uploadBook.toJSON(),
+                          parameters: book.toJSON(),
                           encoding: JSONEncoding.default)
             .responseJSON { response in
                 switch response.result {
                 case .success:
                     print(response)
+                    (self.uploadBookView.uploadButton as? ActivityButtonView)?.hideLoading()
                 case .failure(let error):
                     print(error)
                 }
+                
+                
         }
     }
 
--- MilenaBooksApp/MilenaBooksApp/Views/UploadBookView.swift
+++ MilenaBooksApp/MilenaBooksApp/Views/UploadBookView.swift
@@ -16,4 +16,12 @@ class UploadBookView: UIView {
     @IBOutlet weak var ratingTextField: UITextField!
     @IBOutlet weak var coverImageUrlTextField: UITextField!
     @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var descriptionTextView: UITextView!
+        {
+        didSet {
+            descriptionTextView.layer.cornerRadius = 5
+            descriptionTextView.layer.borderColor = #colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)
+            descriptionTextView.layer.borderWidth = 1
+        }
+    }
 }
